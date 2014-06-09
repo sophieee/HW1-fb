0bc46841240a42f0500a5d862596540bbbeaa82e
Sophie Tang <sophietang@measurefrom-lm.corp.yahoo.com>
2014-06-08 22:12:39 -0700

    Initial Commit

---
+++ HW1 fbTests/HW1_fbTests.m
@@ -0,0 +1,34 @@
+//
+//  HW1_fbTests.m
+//  HW1 fbTests
+//
+//  Created by Sophie Tang on 6/8/14.
+//  Copyright (c) 2014 Sophie Tang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HW1_fbTests : XCTestCase
+
+@end
+
+@implementation HW1_fbTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
